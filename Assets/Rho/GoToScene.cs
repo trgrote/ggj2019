a646e8f0b30bd4cddc22b86de00c67cc20704226
Taylor Grote <grotese@gmail.com>
2019-01-24 18:13:43 -0600

    Add RHO Utils

---
+++ Assets/Rho/GoToScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace rho
+{
+	public class GoToScene : MonoBehaviour
+	{
+		[SerializeField, Scene] private string sceneName;
+
+		public void OnButtonPressed()
+		{
+			if (sceneName != "")
+			{
+				SceneManager.LoadScene(sceneName);
+			}
+		}
+	}
+}

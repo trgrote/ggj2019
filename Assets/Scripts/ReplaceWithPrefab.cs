b89f42045028695627dc39bc456871cba9919ea8
Taylor Grote <grotese@gmail.com>
2019-01-25 21:25:44 -0600

    Add Breacking Object Prefabs with Castle object

---
+++ Assets/Scripts/ReplaceWithPrefab.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Replaces Current Object with given prefab
+public class ReplaceWithPrefab : MonoBehaviour
+{
+    [SerializeField] GameObject prefab;
+
+    public void Replace()
+    {
+        Destroy(this.gameObject);
+        Instantiate(prefab, transform.position, transform.rotation);
+    }
+}

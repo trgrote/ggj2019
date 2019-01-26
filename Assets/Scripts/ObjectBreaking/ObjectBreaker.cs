296a9630f7b0c105df6280bd8c9c6f2b37915f74
Price Comstock <price.comstock@gmail.com>
2019-01-26 14:24:24 -0600

    ObjectBreaker script works

---
+++ Assets/Scripts/ObjectBreaking/ObjectBreaker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectBreaker : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision) {
+        print("Collided with a breakable object");
+        try
+        {
+            collision.gameObject.GetComponent<BreakableObject>().Break();
+        }
+        catch (System.NullReferenceException)
+        {
+            print("Object not breakable");
+        }
+    }
+}

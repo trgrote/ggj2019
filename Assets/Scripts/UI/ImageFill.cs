590f405ed598f8d7582ee8268a12f3ce313c3ed8
Taylor Grote <grotese@gmail.com>
2019-01-26 10:18:01 -0600

    Add Sound Meter Test scene

---
+++ Assets/Scripts/UI/ImageFill.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class ImageFill : MonoBehaviour
+{
+    Image _image;
+
+    [SerializeField, Range(0,1)]
+    float _fillValue = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _image = GetComponent<Image>();
+        _image.type = Image.Type.Filled;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _image.fillAmount = _fillValue;
+    }
+}

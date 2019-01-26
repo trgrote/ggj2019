d25cab58c6d3887a70f0bef0cf24c940dc72dc9a
Taylor Grote <grotese@gmail.com>
2019-01-26 16:03:57 -0600

    Add test camera motion script

---
+++ Assets/Scripts/RotateAroundTransform.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAroundTransform : MonoBehaviour
+{
+    [SerializeField] Transform _target;
+    [SerializeField] float _speed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_target)
+        {
+            transform.LookAt(_target);
+            transform.Translate(Vector3.right * Time.deltaTime * _speed);
+        }
+    }
+}

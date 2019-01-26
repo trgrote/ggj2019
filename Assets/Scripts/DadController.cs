2ba577071f690b19ae1eeb439bbc0cd221b80d0e
Price Comstock <price.comstock@gmail.com>
2019-01-26 13:16:19 -0600

    movement is very reasonable and very cool

--- Assets/Scripts/DadController.cs
+++ Assets/Scripts/DadController.cs
@@ -5,30 +5,46 @@ using UnityEngine;
 
 public class DadController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    [SerializeField] private float movementSpeed = 5.0f;
-    [SerializeField] private float rotationFactor = 0.5f;
+  // Start is called before the first frame update
+  [SerializeField] private float movementSpeed = 5.0f;
+  [SerializeField] private float rotationFactor = 0.5f;
+  Rigidbody rb;
 
-    void Start()
+  void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+  }
+
+  void Update()
+  {
+    Movement();
+    Rotation();
+  }
+
+  private void Rotation()
+  {
+    Vector2 rotationInput = new Vector2(Input.GetAxisRaw("Right Stick Horizontal"), Input.GetAxisRaw("Right Stick Vertical"));
+    if (rotationInput.sqrMagnitude > 0.0f)
     {
-        
+      float rotationAngle = Vector2.SignedAngle(Vector2.right, rotationInput);
+      Quaternion start = rb.rotation;
+      Quaternion end = Quaternion.AngleAxis(rotationAngle + 180.0f, Vector3.up);
+      rb.MoveRotation(Quaternion.Slerp(start, end, rotationFactor));
+      // transform.rotation = Quaternion.Slerp(start, end, rotationFactor);
+      // transform.rotation = Quaternion.AngleAxis(rotationAngle + 180.0f, Vector3.up);
     }
+  }
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.position += Input.GetAxis("Horizontal") * Vector3.right * Time.deltaTime * movementSpeed;
-        transform.position += Input.GetAxis("Vertical") * Vector3.forward * Time.deltaTime * movementSpeed;
+  private void Movement()
+  {
+    float inputH = Input.GetAxis("Horizontal");
+    float inputV = Input.GetAxis("Vertical");
+    rb.velocity = new Vector3(
+        inputH * movementSpeed,
+        0,
+        inputV * movementSpeed
+    );
 
-        // Vector3 direction = Vector3.right * Input.GetAxisRaw("Right Stick Horizontal") +
-        //     Vector3.forward * Input.GetAxisRaw("Right Stick Vertical");
-        Vector2 rotationInput = new Vector2(Input.GetAxisRaw("Right Stick Horizontal"), Input.GetAxisRaw("Right Stick Vertical"));
-        if (rotationInput.sqrMagnitude >= 0.0f) {
-            float rotationAngle = Vector2.SignedAngle(Vector2.right, rotationInput);
-            // Quaternion start = transform.rotation;
-            // Quaternion end = Quaternion.AngleAxis(rotationAngle + 180.0f, Vector3.up);
-            // transform.rotation = Quaternion.Slerp(start, end, rotationFactor);
-            transform.rotation = Quaternion.AngleAxis(rotationAngle + 180.0f, Vector3.up);
-        }
-    }
+    // rb.AddForce(movement, ForceMode.VelocityChange);
+  }
 }

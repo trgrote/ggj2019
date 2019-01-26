92461f5538a93c6b44cbeb876a3c5a0338b93cdb
Price Comstock <price.comstock@gmail.com>
2019-01-26 11:40:35 -0600

    got some basic dad movement working

--- Assets/Scripts/DadController.cs
+++ Assets/Scripts/DadController.cs
@@ -7,6 +7,8 @@ public class DadController : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] private float movementSpeed = 5.0f;
+    [SerializeField] private float rotationFactor = 0.5f;
+
     void Start()
     {
         
@@ -15,11 +17,18 @@ public class DadController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Horizontal")) {
-            transform.position += Input.GetAxis("Horizontal") * Vector3.right * Time.deltaTime * movementSpeed;
-        }
-        if (Input.GetButton("Vertical")) {
-            transform.position += Input.GetAxis("Vertical") * Vector3.forward * Time.deltaTime * movementSpeed;
+        transform.position += Input.GetAxis("Horizontal") * Vector3.right * Time.deltaTime * movementSpeed;
+        transform.position += Input.GetAxis("Vertical") * Vector3.forward * Time.deltaTime * movementSpeed;
+
+        // Vector3 direction = Vector3.right * Input.GetAxisRaw("Right Stick Horizontal") +
+        //     Vector3.forward * Input.GetAxisRaw("Right Stick Vertical");
+        Vector2 rotationInput = new Vector2(Input.GetAxisRaw("Right Stick Horizontal"), Input.GetAxisRaw("Right Stick Vertical"));
+        if (rotationInput.sqrMagnitude >= 0.0f) {
+            float rotationAngle = Vector2.SignedAngle(Vector2.right, rotationInput);
+            // Quaternion start = transform.rotation;
+            // Quaternion end = Quaternion.AngleAxis(rotationAngle + 180.0f, Vector3.up);
+            // transform.rotation = Quaternion.Slerp(start, end, rotationFactor);
+            transform.rotation = Quaternion.AngleAxis(rotationAngle + 180.0f, Vector3.up);
         }
     }
 }

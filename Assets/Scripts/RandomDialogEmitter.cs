47ccac249272df6205f5ebcaf0663e856e8430ff
Price Comstock <price.comstock@gmail.com>
2019-01-27 13:00:22 -0600

    tony and dialog

---
+++ Assets/Scripts/RandomDialogEmitter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomDialogEmitter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] AudioClip[] dialogOptions;
+    [SerializeField] float extraDelaySeconds = 1f;
+    private int lastChosenIndex;
+    private AudioSource audioPlayer;
+
+    void Start()
+    {
+        lastChosenIndex = -1;
+        audioPlayer = GetComponent<AudioSource>();
+        StartCoroutine(PlayAudioClips());
+    }
+
+    // Update is called once per frame
+    IEnumerator PlayAudioClips()
+    {
+        while (true) {
+            AudioClip dialog = GetRandomDialog();
+            audioPlayer.PlayOneShot(dialog);
+            yield return new WaitForSeconds(dialog.length + extraDelaySeconds);
+        }
+        
+    }
+
+    private AudioClip GetRandomDialog()
+    {
+        int index = Random.Range(0, dialogOptions.Length);
+        if (index == lastChosenIndex) {
+            index = (index + 1) % dialogOptions.Length;
+        }
+        lastChosenIndex = index;
+        return dialogOptions[index];
+    }
+}

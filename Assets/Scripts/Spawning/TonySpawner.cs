f45a826a73ec70ca3b0fce9d1308063c1ab99ecf
Taylor Grote <grotese@gmail.com>
2019-01-27 16:09:24 -0600

    Add Menu input handler

--- Assets/Scripts/Spawning/TonySpawner.cs
+++ Assets/Scripts/Spawning/TonySpawner.cs
@@ -16,61 +16,18 @@ public class TonySpawner : MonoBehaviour
 
     void OnEnable()
     {
-        rho.GlobalEventHandler.Register<MenuLeftEvent>(OnMenuExit);
         rho.GlobalEventHandler.Register<TonyBellucaEnterEvent>(OnTonyBellucaEnter);
-        rho.GlobalEventHandler.Register<MeterDepletedEvent>(OnMeterDepleted);
-        rho.GlobalEventHandler.Register<MeterFilledEvent>(OnMeterFilled);
-        rho.GlobalEventHandler.Register<LevelResetStartEvent>(OnLevelResetStart);
         rho.GlobalEventHandler.Register<LevelResetFinishedEvent>(OnLevelResetFinished);
     }
 
     void OnDisable()
     {
-        rho.GlobalEventHandler.Unregister<MenuLeftEvent>(OnMenuExit);
         rho.GlobalEventHandler.Unregister<TonyBellucaEnterEvent>(OnTonyBellucaEnter);
-        rho.GlobalEventHandler.Unregister<MeterDepletedEvent>(OnMeterDepleted);
-        rho.GlobalEventHandler.Unregister<MeterFilledEvent>(OnMeterFilled);
-        rho.GlobalEventHandler.Unregister<LevelResetStartEvent>(OnLevelResetStart);
         rho.GlobalEventHandler.Unregister<LevelResetFinishedEvent>(OnLevelResetFinished);
     }
-
-    IEnumerator WaitForTonyStartInput()
-    {
-        while (true)
-        {
-            if (Input.GetButtonDown("Jump"))
-            {
-                break;
-            }
-            yield return null;
-        }
-
-        rho.GlobalEventHandler.SendEvent(new TonyBellucaEnterEvent());
-    }
-
-    private void OnLevelResetStart(LevelResetStartEvent evt)
-    {
-        StopCoroutine(nameof(WaitForTonyStartInput));
-    }
-
-    private void OnMeterFilled(MeterFilledEvent evt)
-    {
-        StopCoroutine(nameof(WaitForTonyStartInput));
-    }
-
-    private void OnMeterDepleted(MeterDepletedEvent evt)
-    {
-        StopCoroutine(nameof(WaitForTonyStartInput));
-    }
-
     private void OnTonyBellucaEnter(TonyBellucaEnterEvent evt)
     {
         Spawn();
-    }
-
-    private void OnMenuExit(MenuLeftEvent evt)
-    {
-        StartCoroutine(nameof(WaitForTonyStartInput));
     }
 
     private void OnLevelResetFinished(LevelResetFinishedEvent evt)

92b5c40b40c283989e522950f98ba2b9a60e4c34
Taylor Grote <grotese@gmail.com>
2019-01-27 15:16:03 -0600

    Add Door reseting

---
+++ Assets/Scripts/Spawning/ResetDoor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetDoor : MonoBehaviour
+{
+    Vector3 _initalPosition;
+    Quaternion _initialiRotation;
+
+    [SerializeField, TagSelector] string DoorTag;
+
+    void Start()
+    {
+        var door = GameObject.FindGameObjectWithTag(DoorTag);
+        var rb = door.GetComponent<Rigidbody>();
+        _initalPosition = rb.position;
+        _initialiRotation = rb.rotation;
+    }
+
+    void OnEnable()
+    {
+        rho.GlobalEventHandler.Register<LevelResetFinishedEvent>(OnLevelResetFinished);
+    }
+
+    void OnDisable()
+    {
+        rho.GlobalEventHandler.Unregister<LevelResetFinishedEvent>(OnLevelResetFinished);
+    }
+
+    private void OnLevelResetFinished(LevelResetFinishedEvent evt)
+    {
+        var door = GameObject.FindGameObjectWithTag(DoorTag);
+        var rb = door.GetComponent<Rigidbody>();
+        rb.position = _initalPosition;
+        rb.rotation = _initialiRotation;
+        rb.velocity = Vector3.zero;
+    }
+}

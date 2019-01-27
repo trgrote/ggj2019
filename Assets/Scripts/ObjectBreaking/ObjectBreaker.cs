9e374351f573a0c90cbef2363c83d851d3d31ecd
Taylor Grote <grotese@gmail.com>
2019-01-27 12:57:07 -0600

    Simplify Object Breaker

--- Assets/Scripts/ObjectBreaking/ObjectBreaker.cs
+++ Assets/Scripts/ObjectBreaking/ObjectBreaker.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class ObjectBreaker : MonoBehaviour
 {
-    void OnCollisionEnter(Collision collision) {
-        print("Collided with a breakable object");
-        try
-        {
-            collision.gameObject.GetComponent<BreakableObject>().Break();
-        }
-        catch (System.NullReferenceException)
-        {
-            print("Object not breakable");
-        }
+    void OnCollisionEnter(Collision collision) 
+    {
+        collision.gameObject.GetComponent<BreakableObject>()?.Break();
     }
 }
